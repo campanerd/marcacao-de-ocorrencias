d2723da0846ab518dc2aa880f0dfbb588a5f8885
Davi Campaner <campanerdavi@gmail.com>
2026-02-19 18:22:51 -0300

    extracting column from file

--- filtre_service.py
+++ filtre_service.py
@@ -5,17 +5,37 @@ import os
 
 load_dotenv()
 
-pasta = os.getenv("CAMINHO_BASE")
+pasta_base = os.getenv("CAMINHO_BASE")
 
-if not pasta:
+if not pasta_base:
     raise ValueError("CAMINHO_BASE não encontrado no .env")
 
 # data no formato DD.MM
 data_hoje = datetime.now().strftime("%d.%m")
 
-arquivo = f"BASE DIA FOCOS {data_hoje}.xlsx"
-caminho_arquivo = os.path.join(pasta, arquivo)
+# arquivo de origem
+arquivo_origem = f"BASE DIA FOCOS {data_hoje}.xlsx"
+caminho_origem = os.path.join(pasta_base, arquivo_origem)
 
-print("Tentando abrir:", caminho_arquivo)
+if not os.path.exists(caminho_origem):
+    raise FileNotFoundError(f"Arquivo não encontrado: {caminho_origem}")
 
-df = pd.read_excel(caminho_arquivo)
+# ler apenas a sheet desejada
+df = pd.read_excel(
+    caminho_origem,
+    sheet_name="NU-B123",
+    usecols=["CONTRATO"]
+)
+
+# pasta de destino
+pasta_destino = os.path.join("src", "downloads")
+os.makedirs(pasta_destino, exist_ok=True)
+
+# nome do novo arquivo
+arquivo_saida = "contratos.xlsx"
+caminho_saida = os.path.join(pasta_destino, arquivo_saida)
+
+# salvar novo Excel
+df.to_excel(caminho_saida, index=False)
+
+print("Arquivo criado com sucesso em:", caminho_saida)
